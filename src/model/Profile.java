175dbe7ebe2bf04f1e725585f22235901d8f7d9a
YSRathor <51324805+YSRathor@users.noreply.github.com>
2019-08-16 14:31:45 +0100

    Update Profile.java

--- src/model/Profile.java
+++ src/model/Profile.java
@@ -19,7 +19,6 @@ public class Profile {
 	 * A Boolean value for whether a nickname is to be used or not.
 	 */
 	private Boolean useNickName;
-
 	/**
 	 * A ChoresList object.
 	 */
@@ -29,30 +28,10 @@ public class Profile {
 	 */
 	private int noOfChores;
 	/**
-	 * An int value to store the combined time needed to complete all chores.
-	 */
-	private int totalChoreTime;
-	/**
-	 * An int value to store the average time needed to complete a chore.
-	 */
-	private int averageChoreTime;
-	/**
 	 * A String value to store the filepath to where the text file will be created
 	 * and written to.
 	 */
 	private String filePath;
-	/**
-	 * A String value to represent a (-) division of a dynamic length.
-	 */
-	private String separator;
-	/**
-	 * A String value to represent a (-) division of a fixed length.
-	 */
-	private String separator2;
-	/**
-	 * A String value to represent the title line for a text file.
-	 */
-	private String titleLine;
 	/**
 	 * A String value to represent the current Date line for a text file.
 	 */
@@ -62,9 +41,39 @@ public class Profile {
 	 */
 	private String wcDateLine;
 	/**
+	 * A String value to represent the title line for a text file.
+	 */
+	private String titleLine;
+	/**
+	 * A String value to represent a (-) division of a dynamic length, calculated
+	 * from titleLine's length.
+	 */
+	private String separator;
+	/**
+	 * A String value to represent a (-) division of a dynamic length, calculated
+	 * from formatted time String lengths.
+	 */
+	private String separator2;
+	/**
 	 * A String value to represent the total line for a text file.
 	 */
 	private String totalLine;
+	/**
+	 * An int value to store the combined time needed to complete all chores.
+	 */
+	private int totalChoreTime;
+	/**
+	 * An int value to store the average time needed to complete a chore.
+	 */
+	private int averageChoreTime;
+	/**
+	 * A String value to store the formatted combined time.
+	 */
+	private String formattedTotalTime;
+	/**
+	 * A String value to store the formatted average time.
+	 */
+	private String formattedAverageTime;
 
 	// Constructor.
 	/**
@@ -89,15 +98,17 @@ public class Profile {
 		this.useNickName = useNickName;
 		chores = new ChoresList();
 		noOfChores = 0;
-		totalChoreTime = 0;
-		averageChoreTime = 0;
 		filePath = "C:/Users/Admin/Chores for " + person.getFirstName() + ".txt";
-		separator = "";
-		separator2 = "-----------------------------------";
-		titleLine = "\r\n" + person.getFirstName() + "'s Chores for Week " + HelperMethods.getWeek() + ":\r\n";
 		currentDateLine = "Date: " + HelperMethods.getCurrentDate() + "\r\n";
 		wcDateLine = "W/C:  " + HelperMethods.getCurrentWCDate() + "\r\n";
+		titleLine = "\r\n" + person.getFirstName() + "'s Chores for Week " + HelperMethods.getWeek() + ":\r\n";
+		separator = "";
+		separator2 = "";
 		totalLine = "";
+		totalChoreTime = 0;
+		averageChoreTime = 0;
+		formattedTotalTime = "";
+		formattedAverageTime = "";
 	}
 
 	// Methods.
@@ -119,17 +130,65 @@ public class Profile {
 	 * @param i
 	 *            number of chores to be assigned to {@link noOfChores} field.
 	 */
-	public void countChores(int i) {
+	public void setNoOfChores(int i) {
 		noOfChores = i;
 	}
 
 	/**
-	 * Allows a list of chores to be accessed.
+	 * Calculates and stores the value of the combined total chore time.
 	 * 
-	 * @return returns a list of all chores for a profile.
+	 * @param cl
+	 *            the list of chores to be searched through to set a value for
+	 *            totalTime
 	 */
-	public ChoresList getAllChores() {
-		return chores;
+	public void setTotalChoreTime(ChoresList cl) {
+		if (cl.choresListSize() > 0 && cl != null) {
+			for (Chore c : cl) {
+				totalChoreTime += c.getChoreTime();
+			}
+		}
+	}
+
+	/**
+	 * Calculates and stores the value of the average chore time.
+	 * 
+	 * @param totalChoreTime
+	 *            the combined total chore time value.
+	 * @param noOfChores
+	 *            the number of chores value.
+	 */
+	public void setAverageChoreTime(int totalChoreTime, int noOfChores) {
+		if (noOfChores > 0) {
+			averageChoreTime = totalChoreTime / noOfChores;
+		}
+	}
+
+	/**
+	 * Calculates and stores the String value to be outputted for the
+	 * totalChoreTime.
+	 * 
+	 * @param totalChoreTime
+	 *            the combined total chore time value.
+	 */
+	public void setTotalChoreTimeOutput(int totalChoreTime) {
+		formattedTotalTime = "\r\nTotal Time:    " + String.format("%d Hours", totalChoreTime / 60) + " & "
+				+ String.format("%02d Minutes", totalChoreTime % 60);
+	}
+
+	/**
+	 * Calculates and stores the String value to be outputted for the
+	 * averageChoreTime.
+	 * 
+	 * @param averageChoreTime
+	 *            the average chore time value.
+	 */
+	public void setAverageTimeOutput(int averageChoreTime) {
+		if (averageChoreTime > 0) {
+			formattedAverageTime = "\r\nAverage Time:  " + String.format("%d Hours", averageChoreTime / 60) + " & "
+					+ String.format("%02d Minutes", averageChoreTime % 60);
+		} else {
+			formattedAverageTime = "\r\nAverage Time:  " + "0 Hours" + " & " + "00 Minutes";
+		}
 	}
 
 	/**
@@ -142,37 +201,12 @@ public class Profile {
 	}
 
 	/**
-	 * Allows the total time required to complete all chores to be accessed.
+	 * Allows the number of chores allocated to a profile to be accessed.
 	 * 
-	 * @return returns the String of the totalChoreTime in hours and minutes.
+	 * @return returns the number of chores for a profile.
 	 */
-	public String getTotalChoreTimeOutput() {
-		for (Chore c : chores) {
-			totalChoreTime += c.getChoreTime();
-		}
-
-		String formattedTotalTime = "\r\nTotal Time:    " + String.format("%d Hours", totalChoreTime / 60) + " & "
-				+ String.format("%02d Minutes", totalChoreTime % 60);
-
-		return formattedTotalTime;
-	}
-
-	/**
-	 * Allows the average time required to complete a chore to be accessed.
-	 * 
-	 * @return returns the String of the averageChoreTime in hours and minutes.
-	 */
-	public String getAverageChoreTimeOutput() {
-		String formattedAverageTime = "";
-		if (noOfChores != 0) {
-			averageChoreTime = totalChoreTime / noOfChores;
-
-			formattedAverageTime = "\r\nAverage Time:  " + String.format("%d Hours", averageChoreTime / 60) + " & "
-					+ String.format("%02d Minutes", averageChoreTime % 60);
-		} else {
-			formattedAverageTime = "\r\nAverage Time:  " + "0 Hours" + " & " + "00 Minutes";
-		}
-		return formattedAverageTime;
+	public int getNoOfChores() {
+		return noOfChores;
 	}
 
 	/**
@@ -187,6 +221,26 @@ public class Profile {
 		} else {
 			return filePath;
 		}
+	}
+
+	/**
+	 * Allows a profile's current Date line to be accessed.
+	 * 
+	 * @return returns the String for the currentDateLine to be inputed in profile's
+	 *         text file.
+	 */
+	public String getCurrentDateLine() {
+		return currentDateLine;
+	}
+
+	/**
+	 * Allows a profile's week commencing Date line to be accessed.
+	 * 
+	 * @return returns the String for the wcDateLine to be inputed in profile's text
+	 *         file.
+	 */
+	public String getWCDateLine() {
+		return wcDateLine;
 	}
 
 	/**
@@ -214,28 +268,45 @@ public class Profile {
 		for (int i = 0; i < titleLine.trim().length(); i++) {
 			separator += "-";
 		}
-		separator = separator + "\r\n";
+		separator += "\r\n";
 		return separator;
 	}
 
 	/**
-	 * Allows a profile's current Date line to be accessed.
+	 * Allows a list of chores to be accessed.
 	 * 
-	 * @return returns the String for the currentDateLine to be inputed in profile's
-	 *         text file.
+	 * @return returns a list of all chores for a profile.
 	 */
-	public String getCurrentDateLine() {
-		return currentDateLine;
+	public ChoresList getAllChores() {
+		return chores;
 	}
 
 	/**
-	 * Allows a profile's week commencing Date line to be accessed.
+	 * Allows the separator2 String value to be accessed.
 	 * 
-	 * @return returns the String for the wcDateLine to be inputed in profile's text
-	 *         file.
+	 * @return returns a String with a length dependent on the length of the
+	 *         formatted time Strings.
 	 */
-	public String getWCDateLine() {
-		return wcDateLine;
+	public String getSeparator2() {
+
+		if (formattedTotalTime.trim().length() > formattedAverageTime.trim().length()) {
+			for (int i = 0; i < formattedTotalTime.trim().length(); i++) {
+				separator2 += "-";
+			}
+		} else if (formattedAverageTime.trim().length() > formattedTotalTime.trim().length()) {
+			for (int i = 0; i < formattedAverageTime.trim().length(); i++) {
+				separator2 += "-";
+			}
+		} else {
+			int length = formattedTotalTime.trim().length();
+			for (int i = 0; i < length; i++) {
+				separator2 += "-";
+			}
+		}
+
+		separator2 = "\r\n\r\n" + separator2 + "\r\n";
+
+		return separator2;
 	}
 
 	/**
@@ -245,8 +316,46 @@ public class Profile {
 	 *         file.
 	 */
 	public String getTotalLine() {
-		totalLine = "\r\n\r\n" + separator2 + "\r\n" + "Total Chores:  " + noOfChores;
+		totalLine = "Total Chores:  " + noOfChores;
 		return totalLine;
+	}
+
+	/**
+	 * Allows the combined total chore time to be accessed.
+	 * 
+	 * @return returns the totalChoreTime value.
+	 */
+	public int getTotalChoreTime() {
+		return totalChoreTime;
+	}
+
+	/**
+	 * Allows the average chore time to be accessed.
+	 * 
+	 * @return returns the averageChoreTime value.
+	 */
+	public int getAverageChoreTime() {
+		return averageChoreTime;
+	}
+
+	/**
+	 * Allows the total time required to complete all chores to be accessed.
+	 * 
+	 * @return returns the formattedTotalTime, which is the totalChoreTime in hours
+	 *         and minutes.
+	 */
+	public String getTotalChoreTimeOutput() {
+		return formattedTotalTime;
+	}
+
+	/**
+	 * Allows the average time required to complete a chore to be accessed.
+	 * 
+	 * @return returns the formattedAverageTime, which is the averageChoreTime in
+	 *         hours and minutes.
+	 */
+	public String getAverageChoreTimeOutput() {
+		return formattedAverageTime;
 	}
 
 	/**
@@ -254,10 +363,11 @@ public class Profile {
 	 */
 	@Override
 	public String toString() {
-		return "Profile: [person=" + person + ", useNickName=" + useNickName + ", chores=" + chores + ", noOfChores="
-				+ noOfChores + ", totalChoreTime=" + totalChoreTime + ", averageChoreTime=" + averageChoreTime
-				+ ", filePath=" + filePath + ", separator=" + separator + ", separator2=" + separator2 + ", titleLine="
-				+ titleLine + ", currentDateLine=" + currentDateLine + ", wcDateLine=" + wcDateLine + ", totalLine="
-				+ totalLine + "]";
+		return "Profile [person=" + person + ", useNickName=" + useNickName + ", chores=" + chores + ", noOfChores="
+				+ noOfChores + ", filePath=" + filePath + ", currentDateLine=" + currentDateLine + ", wcDateLine="
+				+ wcDateLine + ", titleLine=" + titleLine + ", separator=" + separator + ", separator2=" + separator2
+				+ ", totalLine=" + totalLine + ", totalChoreTime=" + totalChoreTime + ", averageChoreTime="
+				+ averageChoreTime + ", formattedTotalTime=" + formattedTotalTime + ", formattedAverageTime="
+				+ formattedAverageTime + "]";
 	}
 }

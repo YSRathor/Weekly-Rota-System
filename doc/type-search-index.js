61bf9ca5815ad17b7b1fd1c697f26288de8abb78
YSRathor <51324805+YSRathor@users.noreply.github.com>
2019-08-23 12:05:00 +0100

    Delete type-search-index.js

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"model","l":"Chore"},{"p":"model","l":"ChoresAllocation"},{"p":"model","l":"ChoresList"},{"p":"model","l":"Person"},{"p":"model","l":"Profile"},{"p":"model","l":"ProfileData"}]

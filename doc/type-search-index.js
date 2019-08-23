3a89206493fd2c58dd8c247efdc707bd46f99869
YSRathor <51324805+YSRathor@users.noreply.github.com>
2019-08-23 12:05:47 +0100

    Add files via upload

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"model","l":"Chore"},{"p":"model","l":"ChoresAllocation"},{"p":"model","l":"ChoresList"},{"p":"model","l":"Person"},{"p":"model","l":"Profile"},{"p":"model","l":"ProfileData"}]
